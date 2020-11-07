eaa9e0552236748b4b8008510b8ac8eeee4a1937
yiiju <yijuhung0129@gmail.com>
2020-11-07 19:32:18 +0800

    Modify wrong space in setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os
 # Create model path directory
 if not os.path.exists('./modelPath/'):
     os.mkdir('./modelPath/')
-     print("Directory ", './modelPath/',  " Created")
+    print("Directory ", './modelPath/',  " Created")
 
 # Create result inages directory
 if not os.path.exists('./resultImg/'):
     os.mkdir('./resultImg/')
-     print("Directory ", './resultImg/',  " Created")
+    print("Directory ", './resultImg/',  " Created")
 
 # Create test result directory
 if not os.path.exists('./testResult/'):
     os.mkdir('./testResult/')
-     print("Directory ", './testResult/',  " Created")
+    print("Directory ", './testResult/',  " Created")
